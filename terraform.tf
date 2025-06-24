8526ca9da515d781e174c7024283e8e9e63baf98
Khaled1771 <khhaledd.77@gmail.com>
2025-06-24 17:50:25 +0300

    Change Backend with s3 & DynamoDB to lock tfstate

--- terraform.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-lock01"
-    key            = "terraform.tfstate"
-    region         = "me-south-1"
-    dynamodb_table = "terraform-locking"
-  }
-}
